3b3d82cf395dd5bcf5cf7203e37d01495d3931f0
ErikBurdett <erikaburdet@gmail.com>
2022-07-16 13:42:44 -0500

    adds in spellbook - needs to be rewritten for D&D API

---
+++ pages/components/Spell.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import articleStyles from '/styles/Article.module.css'
+
+const ArticleItem = ({ article }) => {
+  return (
+    <Link href={`/article/${article.id}`}>
+      <a className={articleStyles.card}>
+        <Image src={article.cover}
+        width='100%'
+        height='100%'
+        alt={article.coveralt}
+        layout='responsive'/>
+        <h3>{article.title} &rarr;</h3>
+        <p>{article.excerpt}</p>
+      </a>
+    </Link>
+  )
+}
+
+export default ArticleItem
